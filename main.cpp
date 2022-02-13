69c9387f31d8f5ea1a3aedef19e02e3620d12e93
granddaifuku <daifukuformac@gmail.com>
2022-02-14 00:04:15 +0900

    feat: 並行方向にしきりを作成した

--- main.cpp
+++ main.cpp
@@ -17,34 +17,49 @@ __attribute__((unused)) const ll MOD = 1000000007;
 const int WIDTH = 30;
 const int TURN = 300;
 
+// U D L R
 const int dx[4] = {-1, 1, 0, 0};
 const int dy[4] = {0, 0, -1, 1};
 
 enum Objects {
-  None,
+  Human,
   Cow,
   Pig,
   Rabbit,
   Dog,
   Cat,
-  Human,
   Partition,
+  FuturePartition,
+  FutureMove,
 };
-vector<vector<int>> field(WIDTH, vector<int>(WIDTH, Objects::None));
+
+int N, M;
+vector<int> hx, hy;
+vector<vector<map<Objects, int>>> field(WIDTH,
+                                        vector<map<Objects, int>>(WIDTH));
 
 // DEBUG フィールドを出力する
-void output_field() {
-  rep(i, WIDTH) {
-    rep(j, WIDTH) cout << field[i][j];
-    cout << endl;
-  }
-}
+// void output_field() {
+//   rep(i, WIDTH) {
+//     rep(j, WIDTH) cout << field[i][j];
+//     cout << endl;
+//   }
+// }
+
+// フィールドにセットする
+void set_object(int x, int y, Objects obj) { field[x][y][obj]++; }
+
+void unset_object(int x, int y, Objects obj) { field[x][y][obj]--; }
+
+// フィールドに対象がいるかどうかを確認する
+bool contain_object(int x, int y, Objects obj) { return field[x][y][obj] > 0; }
 
 // マスに生物がいるかどうかを判定する
 bool is_contain_lives(int x, int y) {
-  if (field[x][y] == Objects::Human || field[x][y] == Objects::Cow ||
-      field[x][y] == Objects::Pig || field[x][y] == Objects::Rabbit ||
-      field[x][y] == Objects::Dog || field[x][y] == Objects::Cat) {
+  if (field[x][y][Objects::Human] > 0 || field[x][y][Objects::Cow] > 0 ||
+      field[x][y][Objects::Pig] > 0 || field[x][y][Objects::Rabbit] > 0 ||
+      field[x][y][Objects::Dog] > 0 || field[x][y][Objects::Cat] > 0 ||
+      field[x][y][Objects::FutureMove] > 0) {
     return true;
   }
 
@@ -53,13 +68,19 @@ bool is_contain_lives(int x, int y) {
 
 // マスに動物がいるかどうかを判定する
 bool is_contain_animals(int x, int y) {
-  if (field[x][y] == Objects::Cow || field[x][y] == Objects::Pig ||
-      field[x][y] == Objects::Rabbit || field[x][y] == Objects::Dog ||
-      field[x][y] == Objects::Cat) {
+  if (field[x][y][Objects::Cow] > 0 || field[x][y][Objects::Pig] > 0 ||
+      field[x][y][Objects::Rabbit] > 0 || field[x][y][Objects::Dog] > 0 ||
+      field[x][y][Objects::Cat] > 0) {
     return true;
   }
 
   return false;
+}
+
+// マスが通行不能かどうかを判定する
+bool is_partition(int x, int y) {
+  return field[x][y][Objects::Partition] > 0 ||
+         field[x][y][Objects::FuturePartition] > 0;
 }
 
 // グリッドを超える場合にはtrueを返し、それ以外はfalseを返す
@@ -73,10 +94,10 @@ bool is_out_of_bounds(int x, int y) {
 
 // しきりを置くことが出来る場合にはtrueを返し、それ以外はfalseを返す
 bool can_place_partition(int x, int y) {
-  if (field[x][y] == Objects::Human || field[x][y] == Objects::Cow ||
-      field[x][y] == Objects::Pig || field[x][y] == Objects::Rabbit ||
-      field[x][y] == Objects::Dog || field[x][y] == Objects::Cat ||
-      field[x][y] == Objects::Partition) {
+  if (is_contain_lives(x, y)) {
+    return false;
+  }
+  if (is_partition(x, y)) {
     return false;
   }
 
@@ -87,9 +108,7 @@ bool can_place_partition(int x, int y) {
     if (is_out_of_bounds(nx, ny)) {
       continue;
     }
-    if (field[nx][ny] == Objects::Cow || field[nx][ny] == Objects::Pig ||
-        field[nx][ny] == Objects::Rabbit || field[nx][ny] == Objects::Dog ||
-        field[nx][ny] == Objects::Cat) {
+    if (is_contain_animals(nx, ny)) {
       ok = false;
       break;
     }
@@ -98,25 +117,80 @@ bool can_place_partition(int x, int y) {
   return ok;
 }
 
+void apply(string s) {
+  assert((int)s.size() == M);
+  rep(i, M) {
+    if (s[i] == 'U') {
+      unset_object(hx[i], hy[i], Objects::Human);
+      hx[i] += dx[0];
+      hy[i] += dy[0];
+      set_object(hx[i], hy[i], Objects::Human);
+    } else if (s[i] == 'D') {
+      unset_object(hx[i], hy[i], Objects::Human);
+      hx[i] += dx[1];
+      hy[i] += dy[1];
+      set_object(hx[i], hy[i], Objects::Human);
+    } else if (s[i] == 'L') {
+      unset_object(hx[i], hy[i], Objects::Human);
+      hx[i] += dx[2];
+      hy[i] += dy[2];
+      set_object(hx[i], hy[i], Objects::Human);
+    } else if (s[i] == 'R') {
+      unset_object(hx[i], hy[i], Objects::Human);
+      hx[i] += dx[3];
+      hy[i] += dy[3];
+      set_object(hx[i], hy[i], Objects::Human);
+    } else if (s[i] == 'u') {
+      set_object(hx[i] - 1, hy[i], Objects::Partition);
+    } else if (s[i] == 'd') {
+      set_object(hx[i] + 1, hy[i], Objects::Partition);
+    } else if (s[i] == 'l') {
+      set_object(hx[i], hy[i] - 1, Objects::Partition);
+    } else if (s[i] == 'r') {
+      set_object(hx[i], hy[i] + 1, Objects::Partition);
+    } else if (s[i] != '.') {
+      exit(1);
+    }
+  }
+  rep(i, WIDTH) {
+    rep(j, WIDTH) {
+      field[i][j][Objects::FuturePartition] = 0;
+      field[i][j][Objects::FutureMove] = 0;
+    }
+  }
+}
+
 int main() {
   cin.tie(nullptr);
   ios::sync_with_stdio(0);
   // Input
-  int N, M;
   cin >> N;
-  vector<int> px(N), py(N), pt(N);
+  vector<int> px(N), py(N);
+  vector<Objects> pt(N);
   rep(i, N) {
-    cin >> px[i] >> py[i] >> pt[i];
+    int type;
+    cin >> px[i] >> py[i] >> type;
     px[i]--, py[i]--;
-    field[px[i]][py[i]] = pt[i];
+    if (type == 1) {
+      pt[i] = Objects::Cow;
+    } else if (type == 2) {
+      pt[i] = Objects::Pig;
+    } else if (type == 3) {
+      pt[i] = Objects::Rabbit;
+    } else if (type == 4) {
+      pt[i] = Objects::Dog;
+    } else {
+      pt[i] = Objects::Cat;
+    }
+    set_object(px[i], py[i], pt[i]);
   }
   cin >> M;
-  vector<int> hx(M), hy(M);
+  hx.resize(M), hy.resize(M);
   vector<int> dhx(M, 1), dhy(M, 1);  //方向を表す
   rep(i, M) {
     cin >> hx[i] >> hy[i];
     hx[i]--, hy[i]--;
-    field[hx[i]][hy[i]] = Objects::Human;
+    set_object(hx[i], hy[i], Objects::Human);
   }
 
   rep(_, TURN) {
@@ -126,8 +200,8 @@ int main() {
       // 下に障害物をおくことができる
       if (!is_out_of_bounds(hx[i] + 1, hy[i]) &&
           can_place_partition(hx[i] + 1, hy[i])) {
-        field[hx[i] + 1][hy[i]] = Objects::Partition;
         move[i] = 'd';
+        set_object(hx[i] + 1, hy[i], Objects::FuturePartition);
         continue;
       }
       // 左右に移動する
@@ -149,20 +223,26 @@ int main() {
           move[i] = 'R';
         }
       }
-      if (field[hx[i]][ny] == Objects::Partition) {
+      if (is_partition(hx[i], ny)) {
         move[i] = '.';
-      } else {
-        field[hx[i]][hy[i]] = Objects::None;
-        field[hx[i]][ny] = Objects::Human;
-        hy[i] = ny;
+      }
+      if (move[i] == 'U') {
+        set_object(hx[i] - 1, hy[i], Objects::FutureMove);
+      } else if (move[i] == 'D') {
+        set_object(hx[i] + 1, hy[i], Objects::FutureMove);
+      } else if (move[i] == 'L') {
+        set_object(hx[i], hy[i] - 1, Objects::FutureMove);
+      } else if (move[i] == 'R') {
+        set_object(hx[i], hy[i] + 1, Objects::FutureMove);
       }
     }
     cout << move << endl;
+    apply(move);
 
     rep(i, N) {
       string pets;
       cin >> pets;
-      field[px[i]][py[i]] = -1;
+      unset_object(px[i], py[i], pt[i]);
       rep(j, pets.size()) {
         if (pets[j] == 'U') {
           px[i] += dx[0];
@@ -178,7 +258,7 @@ int main() {
           py[i] += dy[3];
         }
       }
-      field[px[i]][py[i]] = pt[i];
+      set_object(px[i], py[i], pt[i]);
     }
   }
 
